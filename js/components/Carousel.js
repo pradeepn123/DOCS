2d392b994d072b310fa8ccd782d4e36c72988e12
Brijesh Patil <brijesh@shoptrade.co>
2024-04-04 16:04:16 +0530

    Carousel component

--- js/components/Carousel.js
+++ js/components/Carousel.js
@@ -1,31 +1,31 @@
 
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { register } from 'swiper/element/bundle'
 
 
-  export default ({slides}) => {
+export default ({ slides }) => {
     register();
-    useEffect(()=>{
+    useEffect(() => {
         const swiperEl = document.querySelector("swiper-container");
-
         const params = {
-          slidesPerView: 1
+            slidesPerView: 1
         };
-    
+
         Object.assign(swiperEl, params);
         swiperEl.initialize();
-    
+
         swiperEl.addEventListener('swiperafterinit', () => {
-          console.log("swiper init");
+            console.log("swiper init");
         });
-    },[])
-  
-return(
-<swiper-container init="false">
-{slides.map((slide, index) =>{
- return <swiper-slide key={index}>
-   {slide}
- </swiper-slide>
-})}
-</swiper-container>
-)}
+    }, [])
+
+    return (
+        <swiper-container init="false">
+            {slides.map((slide, index) => {
+                return <swiper-slide key={index}>
+                    {slide}
+                </swiper-slide>
+            })}
+        </swiper-container>
+    )
+}
