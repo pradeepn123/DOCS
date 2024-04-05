235c35c959667d47680bdf22c2ae2c38716bcf2c
Pradeepkumar Neginhal <58805938+pradeepneginhal@users.noreply.github.com>
2024-04-05 12:00:03 +0530

    Json/prod card (#26)
    * Image with text added two different classes
    
    * Removed errors
    
    * removed console change
    
    ---------
    
    Co-authored-by: Pradeepkumar Neginhal <shoptradepradeep@Pradeepkumars-MacBook-Pro.local>

--- js/components/sections/Banner.js
+++ js/components/sections/Banner.js
@@ -6,8 +6,7 @@ export default ({ data }) => {
     const slides =  Object.values(data).map((eachData, index) => <ImageWithText  data={eachData} key={eachData.name + index} wrapperclass="slideshow-slide__text_wrapper" />);
     const settings = {
         "slidesPerView": 1
-    }
-    console.log("iiii", data)
+    }    
     return (
         <div className="banner__container">
             <div className="banner__header-content">
