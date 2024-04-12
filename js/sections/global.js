ee5b487c837c7019e224e478e1177154584f0710
JingleBaba <umesh@shoptrade.co>
2024-04-12 18:39:40 +0530

    merged with nav menu

--- js/sections/global.js
+++ js/sections/global.js
@@ -1,4 +1,5 @@
 import subscription from "./subscription";
+import customStoreLocatore from './custom-store-locatore';
 import megamenunav from "./megamenu-nav";
 import Megamenuproduct from "./megamenu-product-nav";
 
